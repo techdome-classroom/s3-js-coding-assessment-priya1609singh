4ef0b70ea6a02128c0df916e5224792ff2fde8ee
priya1609singh <130379489+priya1609singh@users.noreply.github.com>
2024-10-07 15:16:25 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,37 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
     
+ 
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    
+  
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+      
+        if (char in map) {
+            
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            
+           
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+          
+            stack.push(char);
+        }
+    }
+    
+   
+    return stack.length === 0;
+};
 };
 
 module.exports = { isValid };
